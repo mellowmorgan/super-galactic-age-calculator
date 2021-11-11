4322d71385eeb16f89b2a0c9b96c308a18d6864b
mellowmorgan <morgan.waites@gmail.com>
2021-11-11 08:40:39 -0800

    Add test for ageCalculator to return Mercury age

--- __test__/galacticCalculator.test.js
+++ __test__/galacticCalculator.test.js
@@ -5,8 +5,21 @@ describe('GalacticCalculator', () => {
   beforeEach(() => {
     cal = new GalacticCalculator(28);
   });
-  test('Should create GalacticCalculator cal object with age property', () => {
-    expect(cal.age).toEqual(28);
+  test('Should return age in Mercury years when ageCalculator("Mercury") is called', () => {
+    expect( cal.ageCalculator("Mercury")).toEqual(116.66666666666667);
   });
+  // test('Should create GalacticCalculator cal object with age property', () => {
+  //   expect(cal.age).toEqual(28);
+  // });
+  // test('Should create GalacticCalculator cal object with age property', () => {
+  //   expect(cal.age).toEqual(28);
+  // });
+  // test('Should create GalacticCalculator cal object with age property', () => {
+  //   expect(cal.age).toEqual(28);
+  // });
+  // test('Should create GalacticCalculator cal object with age property', () => {
+  //   expect(cal.age).toEqual(28);
+  // });
+
   
 });
