4ea2270d1a65f21de19cf64ab336c450c1e12a53
mellowmorgan <morgan.waites@gmail.com>
2021-11-11 08:46:43 -0800

    add method ageCalculator case Mercury test passed

--- __test__/galacticCalculator.test.js
+++ __test__/galacticCalculator.test.js
@@ -8,9 +8,9 @@ describe('GalacticCalculator', () => {
   test('Should return age in Mercury years when ageCalculator("Mercury") is called', () => {
     expect( cal.ageCalculator("Mercury")).toEqual(116.66666666666667);
   });
-  // test('Should create GalacticCalculator cal object with age property', () => {
-  //   expect(cal.age).toEqual(28);
-  // });
+  test('Should return age in Venus years when ageCalculator("Venus") is called', () => {
+    expect( cal.ageCalculator("Venus")).toEqual(?);
+  });
   // test('Should create GalacticCalculator cal object with age property', () => {
   //   expect(cal.age).toEqual(28);
   // });
