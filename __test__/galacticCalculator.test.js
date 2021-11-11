2024ccfb3111f2824ad62a54ce4f632e8cd0040b
mellowmorgan <morgan.waites@gmail.com>
2021-11-11 08:54:47 -0800

    add test for case Jupiter

--- __test__/galacticCalculator.test.js
+++ __test__/galacticCalculator.test.js
@@ -11,12 +11,12 @@ describe('GalacticCalculator', () => {
   test('Should return age in Venus years when ageCalculator("Venus") is called', () => {
     expect( cal.ageCalculator("Venus")).toEqual(45.16129032258065);
   });
-  test('Should return age in Mars years when ageCalculator("Venus") is called', () => {
+  test('Should return age in Mars years when ageCalculator("Mars") is called', () => {
     expect( cal.ageCalculator("Mars")).toEqual(14.893617021276597);
   });
-  // test('Should return age in Venus years when ageCalculator("Venus") is called', () => {
-  //   expect( cal.ageCalculator("Venus")).toEqual(45.16129032258065);
-  // });
+  test('Should return age in Jupiter years when ageCalculator("Jupiter") is called', () => {
+    expect( cal.ageCalculator("Jupiter")).toEqual(28/11.86);
+  });
   // test('Should return age in Venus years when ageCalculator("Venus") is called', () => {
   //   expect( cal.ageCalculator("Venus")).toEqual(45.16129032258065);
   // });
