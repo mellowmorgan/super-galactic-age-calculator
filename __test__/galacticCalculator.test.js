799212608ea5c5c3f3cdaebabf0a2740f6547a0c
mellowmorgan <morgan.waites@gmail.com>
2021-11-11 09:12:54 -0800

    Add test case unknown planet for yearLeft

--- __test__/galacticCalculator.test.js
+++ __test__/galacticCalculator.test.js
@@ -20,8 +20,11 @@ describe('GalacticCalculator', () => {
   test('Should return false when unknown planet is passed as argumument when ageCalculator() is called', () => {
     expect( cal.ageCalculator("Saturn")).toEqual(false);
   });
-  test('Should return years left on Mercury when yearsLeft() is called', () => {
+  test('Should return years left on Mercury when yearsLeft("Mercury") is called', () => {
     expect( cal.yearsLeft("Mercury")).toEqual(72.74/.24-cal.ageCalculator("Mercury"));
+  });
+  test('Should return false when yearsLeft() on unknown is called', () => {
+    expect( cal.yearsLeft("Saturn")).toEqual(false);
   });
   // test('Should return years left on Mercury when yearsLeft() is called', () => {
   //   expect( cal.ageCalculator("Mercury")).toEqual();
